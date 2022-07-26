60f872d30511c957073e9eb96158cb6cabaeb416
MohamedIrfanAM <i4irfan777@gmail.com>
2022-07-26 15:22:18 +0530

    doc_parser include other colors for highlights and include note along with highlights

--- doc_parser.py
+++ doc_parser.py
@@ -30,22 +30,41 @@ class document:
                 pass
             try:
                 highlight = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][1]["content"][0]["paragraph"]["elements"][0]["textRun"]["content"][0:-1]
+                note = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][1]["content"][2]["paragraph"]["elements"][0]["textRun"]["content"][0:-1]
                 highlight = re.sub("\xa0+", "", highlight)
                 color = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][1]["content"][0]["paragraph"]["elements"][0]["textRun"]["textStyle"]["backgroundColor"]["color"]["rgbColor"]["red"]
                 date = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][1]["content"][2]["paragraph"]["elements"][0]["textRun"]["content"][0:-1]
                 pageNo = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][2]["content"][0]["paragraph"]["elements"][0]["textRun"]["content"]
                 url = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][2]["content"][0]["paragraph"]["elements"][0]["textRun"]["textStyle"]["link"]["url"]
+                
+                regex_find_date = re.compile(r'\w{3,9}\W\d{1,2},\W\d{4}')
+                if regex_find_date.search(note):
+                    note = None
+                else:
+                    date = self.json["body"]["content"][i]["table"]["tableRows"][0]["tableCells"][0]["content"][1]["table"]["tableRows"][0]["tableCells"][1]["content"][4]["paragraph"]["elements"][0]["textRun"]["content"][0:-1]
+
+                if color == 1:
+                    color = "red"
+                elif color == 0.5764706:
+                    color = "blue"
+                elif color == 0.77254903:
+                    color = "green"
+                elif color == 0.99215686:
+                    color = "yellow"
 
                 if highlight and highlight != "\n":
                     highlight_data = {
                         "text":highlight,
+                        "note":note,
                         "date":date,
                         "pageNo":pageNo,
-                        "url":url
+                        "url":url,
+                        "color":color
                     }
                     if color == 1:
                         self.new_words[len(self.chapters)-1].append(highlight_data)
-                    elif color == 0.5764706:
+                    else:
                         self.highlights[len(self.chapters)-1].append(highlight_data)
             except:
                 pass
+
