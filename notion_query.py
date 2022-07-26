dc2e648eea7092b6c263618001bc364098bd5028
MohamedIrfanAM <i4irfan777@gmail.com>
2022-07-26 14:13:57 +0530

    implementing create page method

--- notion_query.py
+++ notion_query.py
@@ -1,39 +1,63 @@
 import requests
 import json
 
-class database:
-    def __init__(self,docs_id):
-        self.docs_id = docs_id
+class query:
+    def __init__(self):
         secret_file = open('secrets.json') 
         secret_data = json.load(secret_file) 
 
-        key = secret_data['key']
-        database_id = secret_data['database_id']
+        self.key = secret_data['key']
+        self.database_id = secret_data['database_id']
 
         secret_file.close() 
 
-        self.query_url = f"https://api.notion.com/v1/databases/{database_id}/query"
+        self.query_url = f"https://api.notion.com/v1/databases/{self.database_id}/query"
+        self.page_url = "https://api.notion.com/v1/pages" 
 
         self.headers = {
-            'Authorization': f'Bearer {key}',
+            'Authorization': f'Bearer {self.key}',
             "Notion-Version": "2022-02-22",
             "Accept": "application/json",
             "Content-Type": "application/json",
         }
-        self.filter = {
+
+    def get_last_sync(self,docs_id):
+        filter_data = {
             "filter": {
                 "property": "docs_id",
                 "rich_text": {
-                    "equals": self.docs_id
+                    "equals": docs_id
                 }
             }
         }
-        self.filter_data = json.dumps(self.filter)
-    def get_last_sync(self):
+        filter_data = json.dumps(filter_data)
         try:
-            response = requests.post(self.query_url,headers=self.headers,data=self.filter_data)
+            response = requests.post(self.query_url,headers=self.headers,data=filter_data)
             last_sync_time = response.json()["results"][0]["properties"]["last_sync"]["date"]["start"]
             return last_sync_time
         except:
             return None
+
+    def create_page(self,icon_url,cover_url,properties):
+        page_data = {
+            "parent": { 
+                "database_id": self.database_id 
+            },
+            "icon": {
+                "external": {
+                    "url": icon_url
+                }
+            },
+            "cover": {
+                "external": {
+                    "url": cover_url
+                }
+            },
+            "properties": properties 
+        }
+        page_data = json.dumps(page_data)
+        try:
+            response = requests.post(self.page_url,headers=self.headers,data=page_data)
+        except:
+            print("create page response error")
 
