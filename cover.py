6c40cdad7fc9419159f5d4956c4ffea314bef6e8
MohamedIrfanAM <i4irfan777@gmail.com>
2022-07-28 12:53:26 +0530

    asynchronous processs for cover editing,encoding,uploading)

--- cover.py
+++ cover.py
@@ -4,18 +4,19 @@ import shutil # save img locally
 import json
 import re
 import base64
+import asyncio
 from io import BytesIO  
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-def add_margin(pil_img, right, left):
+async def add_margin(pil_img, right, left):
     width, height = pil_img.size
     new_width = width + right + left
     result = Image.new("RGBA", (new_width, height),(255,0,0,0))
     result.paste(pil_img, (left, 0))
     return result
 
-def to_base64(image):
+async def to_base64(image):
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue())
@@ -30,7 +31,7 @@ def downlaod(url,file_name):
     else:
         print('Image Couldn\'t be retrieved')
 
-def upload(img_str):
+async def upload(img_str):
     secret_file = open('secrets.json') 
     secret_data = json.load(secret_file) 
     image_host_key = secret_data["image_host_key"]
@@ -51,27 +52,38 @@ def upload(img_str):
         r = s.post(image_host_url,data=data)
         return r.json()["image"]["url"]
 
-def get_url(url):
+async def get_url(url):
+    #ID and file_name
     id = re.search(r"(https://books.google.com/books/content\?id=)(\w+)(&.*)",url).group(2)
     file_name = f"covers/{id}.png"
+    #Downlaod thumbnail
     downlaod(url,file_name)
     thumbnail= Image.open(file_name)
+    #Dimensions
     height = thumbnail.size[1]
     width = thumbnail.size[0]
-
-    margin = int((height-width)/2)
-    icon_image = add_margin(thumbnail,margin,margin)
-    icon_str = to_base64(icon_image) 
-    icon_url = upload(icon_str)
-
-    image_width = int((height*7)/5)
-    margin = int((image_width-width)/1.9)
-    cover_image = add_margin(thumbnail,margin,margin) 
-    cover_str = to_base64(cover_image)
-    cover_url = upload(cover_str)
+    icon_margin = int((height-width)/2)
+    cover_width = int((height*7)/5)
+    cover_margin = int((cover_width-width)/1.09)
+    #Add margins
+    icon_add_margin_task = asyncio.create_task(add_margin(thumbnail,icon_margin,icon_margin))
+    cover_add_margin_task = asyncio.create_task(add_margin(thumbnail,cover_margin,cover_margin))
+    icon_image = await icon_add_margin_task
+    cover_image = await cover_add_margin_task
+    #To base64
+    icon_to_base64_task = asyncio.create_task(to_base64(icon_image))
+    cover_to_base64_task = asyncio.create_task(to_base64(cover_image))
+    icon_str = await icon_to_base64_task 
+    cover_str = await cover_to_base64_task
+    #Upload
+    icon_upload_task = asyncio.create_task(upload(icon_str))
+    cover_upload_task = asyncio.create_task(upload(cover_str))
+    icon_url = await icon_upload_task
+    cover_url = await cover_upload_task 
     
     urls = {
         "cover":cover_url,
         "icon":icon_url
     }
     return urls
+
