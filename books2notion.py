19f29c849725e1a80cee05f3618d31f1748589be
MohamedIrfanAM <i4irfan777@gmail.com>
2022-08-06 22:18:03 +0530

    asynchronous functions

--- books2notion.py
+++ books2notion.py
@@ -1,14 +1,15 @@
 import list_docs
-import notion_query
+from notion_query import notion_client
 from doc_parser import document
 import cover
 from metadata_fetcher import book
-import dictionary
+from dictionary import dictionary_class
 from datetime import datetime,timedelta
 import logging
 import sys
 import re
 import argparse
+import asyncio
 
 logging.basicConfig(
         level=logging.DEBUG, 
@@ -20,13 +21,15 @@ logging.basicConfig(
     ])
 logger = logging.getLogger()
 
-
 parser = argparse.ArgumentParser(description='Sync Google Books Highlights to Notion.')
 parser.add_argument('-m', '--mode', choices=["sync","append"],default='append', help="Select mode of syncing")
 args = parser.parse_args()
 
-def validate_time_diff(doc):
-    notion_last_sync_info = notion_query.get_last_sync(doc["docs_id"])
+dictionary = dictionary_class()
+notion_query = notion_client()
+
+async def validate_time_diff(doc):
+    notion_last_sync_info = await notion_query.get_last_sync(doc["docs_id"])
     if notion_last_sync_info is not None:
         logger.info(f"Found existing notion page having docs_id - {doc['docs_id']}")
         notion_last_sync_time = notion_last_sync_info["last_sync_time"]
@@ -56,10 +59,10 @@ def validate_time_diff(doc):
         return None
 
 
-def main():
+async def main():
     docs = list_docs.ids()
     for doc in docs:
-        last_sync_response = validate_time_diff(doc)
+        last_sync_response = await validate_time_diff(doc)
         if last_sync_response is not False:
             parsed_document = None
             page_id = ""
@@ -68,7 +71,7 @@ def main():
                 page_id = last_sync_response["page_id"]
                 page_id = re.sub("-","",str(page_id))
                 if args.mode == 'sync':
-                    notion_query.clear_page_content(page_id)
+                    await notion_query.clear_page_content(page_id)
                     parsed_document = document(doc["docs_id"])
                 else:
                     progress_no = last_sync_response["progress_no"]
@@ -77,7 +80,7 @@ def main():
                     parsed_notes = last_sync_response["parsed_notes"]
                     parsed_new_words = last_sync_response["parsed_new_words"]
                     parsed_document = document(doc["docs_id"],progress_no,parsed_chapters,parsed_highlights,parsed_notes,parsed_new_words)
-                new_words_id = notion_query.get_new_words_id(page_id)
+                new_words_id = await notion_query.get_new_words_id(page_id)
                 new_words_id = re.sub("-","",str(new_words_id))
             else:
                 parsed_document = document(doc["docs_id"])
@@ -85,37 +88,37 @@ def main():
                 urls = cover.get_url(metadata.thumbnail)
                 properties = notion_query.get_page_properties(parsed_document,metadata,doc["docs_id"])
                 children = notion_query.get_header_children(metadata)
-                page_id = str(notion_query.create_page(urls,properties,children))
+                page_id = str( await notion_query.create_page(urls,properties,children))
                 page_id = re.sub("-","",str(page_id))
                 new_words_id = notion_query.create_new_words_database(page_id)
                 new_words_id = re.sub("-","",str(new_words_id))
 
             for new_word in parsed_document.new_words:
-                if args.mode == 'sync':
-                    if not notion_query.new_word_exists(new_words_id,new_word):
+                    if args.mode == 'sync':
+                        if not await notion_query.new_word_exists(new_words_id,new_word):
+                                definition = dictionary.get_definitions(new_word['text'])
+                                await notion_query.add_new_word(new_words_id,new_word,definition)
+                    elif args.mode == 'append':
                         definition = dictionary.get_definitions(new_word['text'])
-                        notion_query.add_new_word(new_words_id,new_word,definition)
-                elif args.mode == 'append':
-                    definition = dictionary.get_definitions(new_word['text'])
-                    notion_query.add_new_word(new_words_id,new_word,definition)
+                        await notion_query.add_new_word(new_words_id,new_word,definition)
 
             for i in range(len(parsed_document.chapters)):
                 chapter = parsed_document.chapters[i]
                 if len(chapter["title"]) > 0:
-                    notion_query.append_chapter(page_id,chapter)
+                    await notion_query.append_chapter(page_id,chapter)
                 highlight_children = []
                 for highlight in parsed_document.highlights[i]:
                     highlight_data_blocks = notion_query.get_highlight_blocks(highlight)
                     highlight_children.extend(highlight_data_blocks)
-                notion_query.append_highlights(page_id,highlight_children)
+                await notion_query.append_highlights(page_id,highlight_children)
 
             utc_now = datetime.utcnow()
             ist_now = str(utc_now + timedelta(hours=5,minutes=30))
             ist_now = (re.sub(' ','T',ist_now[:-3]))+"+05:30"
-            notion_query.update_properties(page_id,parsed_document,ist_now)
+            await notion_query.update_properties(page_id,parsed_document,ist_now)
         else:
             logger.info(f"Document({doc['docs_id']}) highlights and notes are already synced with notion ")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
